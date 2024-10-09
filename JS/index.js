de225a6aa84ccee5b4cdc3dfff0c74f4250fbf22
Kartik <mittalkartik524@gmail.com>
2024-10-09 15:09:13 +0530

    modified js file

---
+++ JS/index.js
@@ -0,0 +1,31 @@
+// client side scripting
+// server side scripting
+
+// HOW TO CREATE VARIABLES IN jS
+// var, let, const
+// var a=12;
+// console.log(a)
+// let b=13;
+// if(b>10){
+//     b=30
+//     console.log(b);
+    
+// }
+// console.log(b);
+// const c=13;
+// if(c>10){
+//     c=30
+//     console.log(c);
+    
+// }
+// console.log(c);
+
+// Datatypes NumberString Boolean NULL BigInt Symbol undefined
+// console.log(Number.MAX_VALUE)
+// console.log(BigInt.MAX_VALUE)
+
+// let x=console.log(Symbol.keyFor(3));
+
+
+
+
---
+++ JS/tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+console.log(Number.MAX_VALUE)
+// console.log(BigInt.MAX_VALUE)
