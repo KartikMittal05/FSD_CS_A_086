ae73652611312625fbc7c644058f505dc1990c03
Kartik <mittalkartik524@gmail.com>
2024-11-13 15:22:56 +0530

    added react files

---
+++ JS/react-cdn.js
@@ -0,0 +1,7 @@
+const parent=document.getElementById("root");
+const element=React.createElement("h1",{},"Shopping Cart");
+const root=ReactDOM.createElement(parent);
+const item1=React.createElement("li",{},"item1");
+const item2=React.createElement("li",{},"item2");
+const list=React.createElement("ol",{},item1,item2);
+root.render([element,list]);
