06dffe62e589858e349febdb069aac84ff842aba
Kartik <mittalkartik524@gmail.com>
2024-11-17 16:23:51 +0530

    added some js files

--- JS/promise.js
+++ JS/promise.js
@@ -1,17 +1,107 @@
-const promice=new Promise(
-    (resolve,reject)=>{
-        console.log("first promise");
-        // resolve("successfully fetched data");
-        reject("network error");
-        
-    }
-);
-promice.then(
-    (msg)=>console.log(msg)
+function register(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+<<<<<<< HEAD
+            return reject('N/w Error during registration');
+            console.log("register end");
+        },1000);
+    });
+ 
+=======
+             return reject("Network Error");
+            console.log("register end");
+            
+        },4000);
+    })
     
-).catch(
-    (err)=>console.log("Error:",err)
+>>>>>>> 6104aa84dba12fb39f00546e0186ac7993b5e150
     
-);
+}
+function sendemail(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+<<<<<<< HEAD
+            console.log("email send");
+            resolve();
+        },1000);
+    });
+   
+=======
+            console.log("mail send");
+            resolve();
+        },5000);
+    })
+    
+    
+>>>>>>> 6104aa84dba12fb39f00546e0186ac7993b5e150
+}
+function login(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log("login end");
+            resolve();
+<<<<<<< HEAD
+        },1000);
+    });
+     
+}
+function getdata(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log("got user data");
+            resolve();
+        },5000);
+    });    
+=======
+        },6000);
+    })
+    
+    
+}
+function getuserdata(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log("Got User Data");
+            resolve();
+        },5000);
+    })
+    
+    
+>>>>>>> 6104aa84dba12fb39f00546e0186ac7993b5e150
+}
+function displaydata(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+<<<<<<< HEAD
+            console.log("display data");
+            resolve();
+        },1000);
+    });   
+}
 
+register()
+.then(sendemail)
+.then(login)
+.then(getdata)
+.then(displaydata)
+.catch((err)=>{
+    console.log("Error: ",err);
+});
 
+console.log("work other app");
+=======
+            console.log("Display User Data");
+            resolve();
+        },5000);
+    })
+    
+    
+}
+
+register().then(sendemail).then(login)
+          .then(getuserdata)
+          .then(displaydata)
+          .catch((err)=>{
+           console.log("Error:"+err);
+          })
+>>>>>>> 6104aa84dba12fb39f00546e0186ac7993b5e150
--- JS/react-cdn.js
+++ JS/react-cdn.js
@@ -5,3 +5,4 @@ const item1=React.createElement("li",{},"item1");
 const item2=React.createElement("li",{},"item2");
 const list=React.createElement("ol",{},item1,item2);
 root.render([element,list]);
+const item3=React.createElement("li",{},"")
